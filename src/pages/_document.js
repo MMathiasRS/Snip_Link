29b96d31e03e8f043708103978dd143a94d75822
Marcos Matias <75454499+MMathiasRS@users.noreply.github.com>
2023-06-21 23:57:59 -0300

    first commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,12 +5,17 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>LinkSnip - O Melhor Encurtador de Links | Create Next App</title>
+          <title>
+            LinkSnip - O Melhor Encurtador de Links | Create Next App
+          </title>
           <meta
             name="description"
             content="O LinkSnip é o encurtador de links definitivo para otimizar suas estratégias de marketing digital. Crie links curtos e personalizados para melhorar a experiência do usuário. Experimente o LinkSnip hoje mesmo!"
           />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          />
           <link
             rel="icon"
             href="https://media.giphy.com/media/VgCDAzcKvsR6OM0uWg/giphy.gif"
